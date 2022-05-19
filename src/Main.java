b604b17fb3db7abd9898f3509c9f875113d81ffd
0x043 <chasegomes00@gmail.com>
2022-05-19 15:32:37 -0400

    State two defined

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,16 @@ public class Main {
                 new symOb('8', 1, 'R', 3), new symOb('9', 1, 'R', 1),
                 new symOb('~', 1, 'L', 3)};
 
+
+        symOb[] symbolObjectTwo = { new symOb('1', 2, 'R', 4), new symOb('2', 2, 'R', 3),
+                new symOb('3', 2, 'R', 3), new symOb('4', 2, 'R', 3),
+                new symOb('4', 2, 'R', 3), new symOb('6', 2, 'R', 3),
+                new symOb('7', 2, 'R', 3), new symOb('8', 2, 'R', 3),
+                new symOb('9', 2, 'R', 3), new symOb('0', 2, 'L', 3),
+                new symOb('~', 2, 'L', 3)};
+
         stateOb stateOne = new stateOb(1, symbolObjectOne);
+        stateOb stateTwo = new stateOb(1, symbolObjectTwo);
 
         testTape.moveTapePos(stateOne.getMoveDirFromArr(3));
         System.out.println(testTape.getTapeCurrentSymbol());
